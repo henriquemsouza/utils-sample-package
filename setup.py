98f490e32c66a060d87534848d062b5b4765f4c2
henriquemsouza <henrique.m_s@hotmail.com>
2022-02-28 09:35:31 -0300

    feat: initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='utils-sample',
+    version='0.0.1',
+    description='just a small test of a private package',
+    url='git@github.com:henriquemsouza/utils-sample-package.git',
+    author='Henrique Martins de Souza',
+    author_email='henrique.m_s@hotmail.com',
+    license='unlicense',
+    packages=['tils-sample'],
+    zip_safe=False
+)
