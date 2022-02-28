42194c021e207363f9e5a78d7ebfa885ef50743c
henriquemsouza <henrique.m_s@hotmail.com>
2022-02-28 09:42:49 -0300

    docs: init readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# utils-sample-package
+
+just a small test of a python private package
+
+### install:
+
+```bash
+pip install git+ssh://git@github.com/henriquemsouza/utils-sample-package.git
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='utils-sample',
+    name='utilsSample',
     version='0.0.1',
     description='just a small test of a private package',
     url='git@github.com:henriquemsouza/utils-sample-package.git',
     author='Henrique Martins de Souza',
     author_email='henrique.m_s@hotmail.com',
     license='unlicense',
-    packages=['utils-sample'],
+    packages=['utilsSample'],
     zip_safe=False
 )
